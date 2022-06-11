fc4d4dca2e7590391fd6c8848eb977571b591054
mayacdsouza <dsouzam@oregonstate.edu>
2022-06-10 23:51:32 -0500

    Finished week 1 tasks

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile from "../assets/profile.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+             <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              img src = {profile}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Maya D'Souza</div>
+            <div className="brief_description">
+              I am currently studying Computer Science at Oregon State University. Some of my hobbies include hiking, reading, doing logic puzzles, and cooking.
+            </div>
+          </div>
+        </div>
+      </div> 
       </div>
     )
   }
